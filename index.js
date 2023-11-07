e95cd3639ba64b1cb4ca9558f53812b7445440e8
Shawal <shahishawal@gmail.com>
2023-11-07 19:14:11 +0600

    adding put function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
@@ -30,7 +30,7 @@ async function run() {
 
     const database = client.db("JobpostingDB");
     const jobCollection = database.collection("haiku");
-
+    const jobtypeCollection = database.collection("jobtype")
 //    add a job data 
    app.post("/jobs", async(req, res)=>{
     const jobs= req.body
@@ -47,7 +47,53 @@ async function run() {
 
    })
 
+   app.get("/jobs/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = {
+      _id: new ObjectId(id),
+    };
+    const result = await jobCollection.findOne(query);
+    console.log(result);
+    res.send(result);
+  });
+   
+   app.get("/jobs/:jb_category", async(req, res)=>{
+        const jobCat = req.params.jb_category
+        const query = {jb_category:jobCat}
+        const result = await jobCollection.find(query).toArray()
+        res.send(result)
+   })
 
+   app.get("/jobstype", async(req, res)=>{
+    const result = await jobtypeCollection.find().toArray();
+    res.send(result)
+
+   })
+
+  //  updated id 
+  app.put("/jobs/:id", async (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    const filter = { _id: new ObjectId(id) };
+    const updatedUSer = {
+      $set: {
+        job_title: data.job_title,
+        salary_range: data.salary_range,
+        posted_name: data.posted_name,
+        job_applicate_number: data.job_applicate_number,
+        jb_dsc: data.jb_dsc,
+        jb_category:data.jb_category,
+        jb_post_date: data.jb_post_date,
+        app_deadline:data.app_deadline,
+        image:data.image
+      },
+    };
+    const result = await jobCollection.updateOne(
+      filter,
+      updatedUSer
+    );
+    res.send(result);
+  });
 
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
